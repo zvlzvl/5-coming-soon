20f5d35434dc5fa50833f1f58501c7b00ed8c354
zvlzvl <76951647+zvlzvl@users.noreply.github.com>
2021-01-28 16:46:54 +0200

    update clock update clock #35

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
- import { clock } from './components/clock.js' 
+  
+import { clock } from './components/clock.js';
 import { socialsData } from './data/socialsData.js';
 import { renderSocials } from './components/renderSocials.js';
 
-clock('.clock');
+clock('.clock', '01-04 14:00:00');
 renderSocials('footer > .socials', socialsData);
