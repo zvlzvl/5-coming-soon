08537d4ce26289c51b3a92d038e79b979e53b3be
zvlzvl <76951647+zvlzvl@users.noreply.github.com>
2021-02-04 15:12:36 +0200

    Progress bars #17

--- js/components/ProgressBar.js
+++ js/components/ProgressBar.js
@@ -33,8 +33,25 @@ class ProgressBar {
         return true;
     }
 
+    generateProgressBar(progressBar){
+        console.log(progressBar);
+        return `<div class="progress-bar">
+        <div class="texts">
+            <div class = "label">${progressBar.label}</div>
+            <div class = "value">${progressBar.value}%</div>
+            </div>
+            <div class="bar">
+                <div class="progress" style="width: ${progressBar.value}%"></div>
+            </div>     
+    </div>`
+    }
+
     render() {
-        let HTML = 'DEMO PROGRESS BAR CONTENT...';
+        let HTML = '';
+
+        for(const progress of this.data){
+            HTML += this.generateProgressBar(progress);
+        }
 
         this.DOM.innerHTML += HTML;
     }
